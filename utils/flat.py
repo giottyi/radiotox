85c5fbc953bba5fbc1db627c0bebc9ceca1ec457
giottyi <giaiotti.francesco@gmail.com>
2025-05-12 09:26:02 +0200

    added utils

---
+++ utils/flat.py
@@ -0,0 +1,30 @@
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+
+import glob
+import os
+import sys
+
+from utils.get_views import get_views
+
+
+def get_flat(directory):
+    flats_stack = get_views(directory)
+    flat_mean = jnp.mean(flats_stack, axis=0)
+    return flat_mean
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python utils/get_flat.py <directory>")
+
+    directory = sys.argv[1]
+    flat = get_flat(directory)
+    plt.imshow(flat, cmap="gray")
+    plt.axis("off")
+    plt.savefig("flat_mean.tif")
+
+
+if __name__ == "__main__":
+    main()
+
