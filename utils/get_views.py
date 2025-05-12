347bb3664950dc78da468b0317ffe3a3e69ea173
giottyi <giaiotti.francesco@gmail.com>
2025-05-12 09:30:11 +0200

    now added utils

---
+++ utils/get_views.py
@@ -0,0 +1,24 @@
+import cv2 as cv
+import jax.numpy as jnp
+
+import glob
+import os
+
+
+def get_views(directory):
+    views_paths = sorted(glob.glob(os.path.join(directory, "*.tif")))
+    views = []
+
+    for view_path in views_paths:
+        view = cv.imread(view_path, cv.IMREAD_GRAYSCALE)
+        if view is not None:
+            views.append(view)
+        else:
+            print(f"Warning: Failed to read {view_path}")
+
+        if not views:
+            raise ValueError("No valid view images found.")
+
+    views_stack = jnp.stack(views, axis=0)
+    return views_stack
+
